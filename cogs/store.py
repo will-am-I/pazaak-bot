1533f05f122e3073a449b4403a0282fc0ec05c97
Will Reaves <wjreaves93@yahoo.com>
2021-06-15 19:50:46 -0500

    Update store.py

--- cogs/store.py
+++ cogs/store.py
@@ -19,7 +19,7 @@ class Store(commands.Cog):
       else:
          endAt = startAt + 7
       
-      description = "Use p.buy <card> to purchase a card."
+      description = "Use **p.buy <card>** to purchase a card."
       if page == 4:
          description += "\nOnly one of each of these cards per person."
 
